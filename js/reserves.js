a40a344486af5bdaffc60f5f21607e27e14a21e6
HoonDragonite <lsoonnoon@gmail.com>
2021-02-16 14:24:24 +0900

    Edit reserves.js, Calculate total Amt without change history

--- js/reserves.js
+++ js/reserves.js
@@ -1,72 +1,78 @@
+/* LocalStorage Values*/
 let userName = "";
 let contractDate = "";
 let company = "";
-let payDate = "";
+let payDate = ""; // "yyyy-MM-dd"
+let savedChangeDate = "";
 let changeDateArr = [];
-let today = "";
+
+/* Amt */
 let selfAmt = 0;
 let companyAmt = 0;
 let governmentAmt = 0;
+let totalAmt = 0;
+
+/* pointCounting */
+let pointCount = 0;
+const monthCount = 24;
+
 
 getLocalStorageValues();
-setUI();
-calcYMD();
+setComponentValues();
+calculateAmt();
+
 
 function getLocalStorageValues(){
     userName = localStorage.getItem("userName");
     contractDate = localStorage.getItem("contractDate");
     company = localStorage.getItem("company");
     payDate = localStorage.getItem("payDate");
+    savedChangeDate = localStorage.getItem("changeDate");
 
-    let savedChangeDate = localStorage.getItem("changeDate");
     if(savedChangeDate == null){
         console.log("저장된 변경이력이 없습니다.");
         savedChangeDate = "";
     }
-    
-    changeDateArr = savedChangeDate.split(",").filter(n => n);
-
-    for(i in changeDateArr){
-        console.log(i + ", " + changeDateArr[i]);
+    else{
+        changeDateArr = savedChangeDate.split(",").filter(n => n); // ,구분자로 나누어 날짜를 배열에 넣음
+        for(i in changeDateArr){
+            console.log(i + ", " + changeDateArr[i]);
+        }
     }
 }
 
-function setUI(){
+function setComponentValues(){
     $("#userTitle").text(userName + "님 안녕하세요.");
 }
 
-function calcYMD(){
-    let replacedDate = payDate.replace(/-/g, '');
-    alert(replacedDate + ' to ' + getToday());
-}
-
-function getToday(){
-    var date = new Date();
-    var year = date.getFullYear();
-    var month = ("0" + (1 + date.getMonth())).slice(-2);
-    var day = ("0" + date.getDate()).slice(-2);
-
-    return year + month + day;
-}
-
-function calcSelfAmt(){
+function calculateAmt(){
+    let momentPayDate = moment(payDate).format('YYYY-MM-DD');
+    let momentToday = moment().format('YYYY-MM-DD');
     
+    for(let i=0; i<monthCount; i++){
+        if(moment(momentPayDate).isSameOrBefore(momentToday)){
+            if(i+1 == 1){
+                governmentAmt += 800000;
+                companyAmt += 500000;
+            } else if(i+1 == 6){
+                governmentAmt += 1200000;
+                companyAmt += 600000;
+            } else if(i+1 == 12){
+                governmentAmt += 1200000;
+                companyAmt += 600000;
+            } else if(i+1 == 18){
+                governmentAmt += 1400000;
+                companyAmt += 600000;
+            } else if(i+1 == 24){
+                governmentAmt += 1400000;
+                companyAmt += 700000;
+            }
+            selfAmt += 125000;
+
+            pointCount++;
+            momentPayDate = moment(momentPayDate).add(1, 'months').format('YYYY-MM-DD');
+        }
+    }
+
+    totalAmt = selfAmt + companyAmt + governmentAmt;
 }
-
-function calcCompanyAmt(){
-
-}
-
-function calcGovernmentAmt(){
-    
-}
-
-/*
-계산할 것
-
-1. 본인납부금 24회 12.5
-2. 취업지원금 5회  80 120 120 140 140
-3. 기업기여금 5회  50, 60, 60, 60, 70
-
-
-*/
